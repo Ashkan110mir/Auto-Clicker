c8bd6cb85109dbae4d3b35f53def3d3d1d087010
Ashkan <ashkan110mir@gmail.com>
2024-06-18 20:04:54 +0330

    Add project files.

---
+++ clicker/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnclick = new System.Windows.Forms.Button();
+            this.lblnumber = new System.Windows.Forms.Label();
+            this.btnautoclick = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnclick
+            // 
+            this.btnclick.Location = new System.Drawing.Point(616, 220);
+            this.btnclick.Name = "btnclick";
+            this.btnclick.Size = new System.Drawing.Size(142, 82);
+            this.btnclick.TabIndex = 0;
+            this.btnclick.Text = "کلیک کنید";
+            this.btnclick.UseVisualStyleBackColor = true;
+            this.btnclick.Click += new System.EventHandler(this.btnclick_Click);
+            // 
+            // lblnumber
+            // 
+            this.lblnumber.AutoSize = true;
+            this.lblnumber.Location = new System.Drawing.Point(448, 43);
+            this.lblnumber.Name = "lblnumber";
+            this.lblnumber.Size = new System.Drawing.Size(54, 13);
+            this.lblnumber.TabIndex = 1;
+            this.lblnumber.Text = "تعداد کلیک";
+            // 
+            // btnautoclick
+            // 
+            this.btnautoclick.Location = new System.Drawing.Point(78, 268);
+            this.btnautoclick.Name = "btnautoclick";
+            this.btnautoclick.Size = new System.Drawing.Size(90, 49);
+            this.btnautoclick.TabIndex = 2;
+            this.btnautoclick.Text = "کلیک اتوماتیک";
+            this.btnautoclick.UseVisualStyleBackColor = true;
+            this.btnautoclick.Click += new System.EventHandler(this.btnautoclick_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(365, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "موقعیت موس";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(365, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "موقعیت موس";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnautoclick);
+            this.Controls.Add(this.lblnumber);
+            this.Controls.Add(this.btnclick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnclick;
+        private System.Windows.Forms.Label lblnumber;
+        private System.Windows.Forms.Button btnautoclick;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ clicker/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace clicker
+{
+    public partial class Form1 : Form
+    {
+        int mainx;
+        int mainy;
+        bool start = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnclick_Click(object sender, EventArgs e)
+        {
+            int number;
+            if (lblnumber.Text == "تعداد کلیک")
+            {
+                number = 0;
+                lblnumber.Text = number.ToString();
+            }
+            else
+            {
+                number = int.Parse(lblnumber.Text);
+                number++;
+                lblnumber.Text = number.ToString();
+            }
+
+        }
+
+        private void btnautoclick_Click(object sender, EventArgs e)
+        {
+           
+
+
+            //Thread trackertread = new Thread(tracker);
+            //label1.Text = Cursor.Position.X.ToString();
+            //label2.Text = Cursor.Position.Y.ToString();
+            //mainx = Cursor.Position.X;
+            //mainy = Cursor.Position.Y;
+            //trackertread.Start();
+
+
+
+
+            //Thread mouse_click = new Thread(mouseclick);
+            //mouse_click.Start();
+
+        }
+        //private void tracker()
+        //{
+        //    while (start == true)
+        //    {
+        //        int pointx = Cursor.Position.X;
+        //        int pointy = Cursor.Position.Y;
+
+        //        label1.Invoke((MethodInvoker)(() => label1.Text = pointx.ToString()));
+        //        label2.Invoke((MethodInvoker)(() => label2.Text = pointy.ToString()));
+        //        Thread.Sleep(2000);
+        //    }
+        //}
+
+        private void press()
+        {
+            while (true)
+            {
+                if (GetAsyncKeyState(resumeid) < 0)
+                {
+                    start = true;
+                    Thread mouse_click = new Thread(mouseclick);
+                    mainx = Cursor.Position.X; 
+                    mainy = Cursor.Position.Y;
+                    mouse_click.Start();
+                }
+                if (GetAsyncKeyState(stopid) < 0)
+                {
+                    start = false;
+                    Thread mouse_click = new Thread(mouseclick);
+                    mouse_click.Abort();
+                }
+            }
+        }
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, IntPtr dxExteraInfo);
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(int vkey);
+
+
+        const uint leftdown = 0x02;
+        const uint leftup = 0x04;
+        const int resumeid = 0x52;
+        const int stopid = 0x53;
+
+        void mouseclick()
+        {
+            while (start == true)
+            {
+                Random rnd = new Random();
+                int randomx = rnd.Next(10, 30);
+                int randomy = rnd.Next(10, 30);
+                int back_or_forward = rnd.Next(1, 3);
+                int x=mainx;
+                int y=mainy;
+                if (back_or_forward == 1)
+                {
+                    x = x - randomx;
+                    y = y - randomy;
+                }
+                else if (back_or_forward == 2)
+                {
+                    x = x + randomx;
+                    y = y + randomy;
+                }
+                Point mainpoint = new Point(mainx, mainy);
+                uint pointx = (uint)(int)mainx;
+                uint pointy = (uint)(int)mainy;
+                Point randompoint = new Point(x, y);
+                Cursor.Position = randompoint;
+                mouse_event(leftdown, 0, 0, 0, IntPtr.Zero);
+                mouse_event(leftup, 0, 0, 0, IntPtr.Zero);
+                Thread.Sleep(2000);
+            }
+
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread start = new Thread(press);
+            start.Start();
+        }
+    }
+}
